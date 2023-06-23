a552fb93a3b57c4160b02fa00e19f66b609613ec
ZiadHesham-99 <76854651+ZiadHesham-99@users.noreply.github.com>
2023-06-23 19:43:30 +0300

    added GP URDF simulation with movemnt
    added urdf movment
    awaiting ROS pub of the 2 motors PWM

---
+++ GP/FireInspectionRover/Library/PackageCache/com.unity.robotics.urdf-importer@90f353e435/Runtime/Controller/JointControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Robotics.UrdfImporter;
+
+public class JointControl : MonoBehaviour
+{
+ 
+
+
+    void Start()
+    {
+
+    }
+
+
+}
